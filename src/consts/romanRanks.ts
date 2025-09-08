97196abf4ca60dbd67cf907a6c49edd98ee22f9f
Kelvin Charles <kelvinc1291@hotmail.com>
2025-09-08 15:27:15 -0300

    feat: add array of roman numerals from 1 to 15

---
+++ src/consts/romanRanks.ts
@@ -0,0 +1,18 @@
+// Array de números romanos de 1 a 15
+export const romanRanks = [
+  "I",
+  "II",
+  "III",
+  "IV",
+  "V",
+  "VI",
+  "VII",
+  "VIII",
+  "IX",
+  "X",
+  "XI",
+  "XII",
+  "XIII",
+  "XIV",
+  "XV",
+];
