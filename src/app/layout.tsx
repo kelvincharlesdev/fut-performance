1c299581326f56033df0f947882664aaf8c71ebd
Kelvin Charles <kelvinc1291@hotmail.com>
2025-09-08 15:22:02 -0300

    fix: rearrange imports and add ToastContainer to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
-import type { Metadata } from 'next';
-import { SidebarProvider } from '@/components/ui/sidebar';
-import { MobileHeader, SidebarDesktop } from '@/content/Globals';
-import './globals.css';
+import { ToastContainer, toast } from "react-toastify";
+import type { Metadata } from "next";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { MobileHeader, SidebarDesktop } from "@/content/Globals";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'FUT Performance',
-  description: 'Ajuda com o desempenho do seu time no FIFA Ultimate Team',
+  title: "FUT Performance",
+  description: "Ajuda com o desempenho do seu time no FIFA Ultimate Team",
 };
 
 export default function RootLayout({
@@ -16,12 +17,12 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body className={`antialiased`}>
+        <ToastContainer />
         <SidebarProvider>
           <SidebarDesktop />
           <main className="w-full bg-background">
             <MobileHeader />
-
-            {children}
+            <div className="p-8">{children}</div>
           </main>
         </SidebarProvider>
       </body>
