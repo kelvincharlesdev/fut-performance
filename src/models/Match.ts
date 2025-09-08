ab8c5fc2fe434fddb161c2615f8b289cf693da68
Kelvin Charles <kelvinc1291@hotmail.com>
2025-09-08 15:24:36 -0300

    feat: add Match interface to model match data

---
+++ src/models/Match.ts
@@ -0,0 +1,12 @@
+export interface Match {
+  opponent: string;
+  plataforma: "PS4" | "PS5" | "XBOX" | "PC";
+  myGoals: string;
+  opponentGoals: string;
+  myPossession: string;
+  opponentPossession: string;
+  myTotalPasses: string;
+  myAccuratePasses: string;
+  dc: "sim" | "nao";
+  resultado: "vitoria" | "derrota";
+}
