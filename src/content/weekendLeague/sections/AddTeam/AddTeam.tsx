7a69d0c2c4cf1b0aa3512eacb2f78b32f5fa58ac
Kelvin Charles <kelvinc1291@hotmail.com>
2025-09-09 17:50:25 -0300

    refactor: comment out unused code in the AddTeam component

--- src/content/weekendLeague/sections/AddTeam/AddTeam.tsx
+++ src/content/weekendLeague/sections/AddTeam/AddTeam.tsx
@@ -1,41 +1,41 @@
-import { PlayersForm } from "@/containers/Forms/PlayersForm";
+// import { PlayersForm } from "@/containers/Forms/PlayersForm";
 
-import {
-  Dialog,
-  DialogTrigger,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-  DialogDescription,
-} from "@/components/ui/dialog";
-import { Button } from "@/components/ui/button";
+// import {
+//   Dialog,
+//   DialogTrigger,
+//   DialogContent,
+//   DialogHeader,
+//   DialogTitle,
+//   DialogDescription,
+// } from "@/components/ui/dialog";
+// import { Button } from "@/components/ui/button";
 
-export const AddTeam = () => {
-  return (
-    <section className="mt-8">
-      <p className="text-xl text-center md:text-left  text-white">
-        Adicione seu time para ter estatisticas de gols e assistencias de cada
-        atleta!
-      </p>
+// export const AddTeam = () => {
+//   return (
+//     <section className="mt-8">
+//       <p className="text-xl text-center md:text-left  text-white">
+//         Adicione seu time para ter estatisticas de gols e assistencias de cada
+//         atleta!
+//       </p>
 
-      <Dialog open={open} onOpenChange={setOpen}>
-        <DialogTrigger asChild>
-          <Button className="mt-4 w-full max-w-2xs cursor-pointer">
-            Adicionar Time!
-          </Button>
-        </DialogTrigger>
+//       <Dialog open={open} onOpenChange={setOpen}>
+//         <DialogTrigger asChild>
+//           <Button className="mt-4 w-full max-w-2xs cursor-pointer">
+//             Adicionar Time!
+//           </Button>
+//         </DialogTrigger>
 
-        <DialogContent className="text-primary-foreground">
-          <DialogHeader>
-            <DialogTitle>Nova Weekend League</DialogTitle>
-            <DialogDescription>
-              Preencha os dados para iniciar sua nova WL.
-            </DialogDescription>
-          </DialogHeader>
+//         <DialogContent className="text-primary-foreground">
+//           <DialogHeader>
+//             <DialogTitle>Nova Weekend League</DialogTitle>
+//             <DialogDescription>
+//               Preencha os dados para iniciar sua nova WL.
+//             </DialogDescription>
+//           </DialogHeader>
 
-          <PlayersForm onSuccess={() => setOpen(false)} />
-        </DialogContent>
-      </Dialog>
-    </section>
-  );
-};
+//           <PlayersForm onSuccess={() => setOpen(false)} />
+//         </DialogContent>
+//       </Dialog>
+//     </section>
+//   );
+// };
